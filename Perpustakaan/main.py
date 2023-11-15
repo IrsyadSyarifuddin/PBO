479e4cfeed6fb1b7e5fcef2f2b23189e08ef7116
IrsyadSyarifuddin <daiciarekmalang@gmail.com>
2023-11-15 11:27:39 +0700

    Update main.py

--- Perpustakaan/main.py
+++ Perpustakaan/main.py
@@ -1,8 +1,8 @@
 from Buku.tampil_info import Tampil_info as Buku
 from Anggota.tampil_info import Tampil_info as Anggota
 
-buku1 = Buku('saya','siapa')
+buku1 = Buku('majalah','pengarang')
 buku1.tampil_info()
 print('\n')
-anggota1 = Anggota('saya',1)
+anggota1 = Anggota('anggota 1',1)
 anggota1.tampil_info()
